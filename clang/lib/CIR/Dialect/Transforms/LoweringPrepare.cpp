24bcb78c9ea3c395ad9383fbaf785bf5dbbcd417
David Rivera <davidriverg@gmail.com>
2026-04-02 18:01:00 -0400

    fix undefined void ty

--- clang/lib/CIR/Dialect/Transforms/LoweringPrepare.cpp
+++ clang/lib/CIR/Dialect/Transforms/LoweringPrepare.cpp
@@ -1750,6 +1750,7 @@ void LoweringPreparePass::buildCUDAModuleCtor() {
   CIRBaseBuilderTy builder(getContext());
   builder.setInsertionPointToStart(mlirModule.getBody());
 
+  Type voidTy = builder.getVoidTy();
   PointerType voidPtrTy = builder.getVoidPtrTy();
   PointerType voidPtrPtrTy = builder.getPointerTo(voidPtrTy);
   IntType intTy = builder.getSIntNTy(32);
