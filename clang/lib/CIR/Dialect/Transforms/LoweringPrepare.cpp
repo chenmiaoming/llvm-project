beb2302b6bdfe37e8ee9b37f991cdc3b654a1e3a
David Rivera <davidriverg@gmail.com>
2026-04-02 15:58:55 -0400

    Fix conflicts and add section to fatbin globals

--- clang/lib/CIR/Dialect/Transforms/LoweringPrepare.cpp
+++ clang/lib/CIR/Dialect/Transforms/LoweringPrepare.cpp
@@ -1750,6 +1750,13 @@ void LoweringPreparePass::buildCUDAModuleCtor() {
 
   // --- Create fatbin globals ---
 
+  // The section names are different for MAC OS X.
+  llvm::StringRef fatbinConstName =
+      astCtx->getLangOpts().HIP ? ".hip_fatbin" : ".nv_fatbin";
+
+  llvm::StringRef fatbinSectionName =
+      astCtx->getLangOpts().HIP ? ".hipFatBinSegment" : ".nvFatBinSegment";
+
   // Create the fatbin string constant with GPU binary contents.
   auto fatbinType =
       ArrayType::get(&getContext(), charTy, gpuBinary->getBuffer().size());
@@ -1760,7 +1767,7 @@ void LoweringPreparePass::buildCUDAModuleCtor() {
   fatbinStr.setAlignment(8);
   fatbinStr.setInitialValueAttr(cir::ConstArrayAttr::get(
       fatbinType, builder.getStringAttr(gpuBinary->getBuffer())));
-  assert(!cir::MissingFeatures::opGlobalSection());
+  fatbinStr.setSection(fatbinConstName);
   fatbinStr.setPrivate();
 
   // Create the fatbin wrapper struct:
@@ -1773,6 +1780,7 @@ void LoweringPreparePass::buildCUDAModuleCtor() {
   GlobalOp fatbinWrapper = GlobalOp::create(
       builder, loc, fatbinWrapperName, fatbinWrapperType,
       /*isConstant=*/true, {}, GlobalLinkageKind::PrivateLinkage);
+  fatbinWrapper.setSection(fatbinSectionName);
 
   constexpr unsigned cudaFatMagic = 0x466243b1;
   constexpr unsigned hipFatMagic = 0x48495046;
